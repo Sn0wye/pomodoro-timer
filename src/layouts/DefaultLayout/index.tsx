16039f8f457b1b5b271679616d60dd465c69d437
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-04 18:47:52 -0300

    feat: create default layout

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+import { Header } from '../../components/Header';
+import { LayoutContainer } from './styles';
+
+interface Props {
+  children?: ReactNode;
+}
+
+export const DefaultLayout = ({ children }: Props) => {
+  return (
+    <LayoutContainer>
+      <Header />
+      {children}
+    </LayoutContainer>
+  );
+};
