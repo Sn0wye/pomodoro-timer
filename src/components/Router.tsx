3223459f96c05053f88eb520c35a31bf2caaa6f5
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-05 21:24:14 -0300

    feat: add router component

---
+++ src/components/Router.tsx
@@ -0,0 +1,13 @@
+import { Route } from 'wouter';
+import { DefaultLayout } from '../layouts/DefaultLayout';
+import { History } from '../pages/History';
+import { Home } from '../pages/Home';
+
+export const Router = () => {
+  return (
+    <DefaultLayout>
+      <Route path='/' component={Home} />
+      <Route path='/history' component={History} />
+    </DefaultLayout>
+  );
+};
