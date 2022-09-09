01ca82f649978b443646027929e8503e1fc9f0c3
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-08 21:57:58 -0300

    feat: separate newCycleForm component from home

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,42 @@
+import { useContext } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import { CyclesContext } from '../..';
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles';
+
+export const NewCycleForm = () => {
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
+
+  return (
+    <FormContainer>
+      <label htmlFor='task'>I will work at</label>
+      <TaskInput
+        id='task'
+        list='task-suggestions'
+        placeholder='Name your task'
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id='task-suggestions'>
+        <option value='Projeto 1' />
+        <option value='Projeto 2' />
+        <option value='Projeto 3' />
+        <option value='Banana' />
+      </datalist>
+      <label htmlFor='minutesAmount'>for</label>
+      <MinutesAmountInput
+        type='number'
+        id='minutesAmount'
+        placeholder='00'
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutes.</span>
+    </FormContainer>
+  );
+};
