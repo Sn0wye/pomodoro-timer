5f8d81e0c872bf9bc551a7907ac1cc6b68701794
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-08 22:23:56 -0300

    feat: add cycles provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components';
 import { Router } from './components/Router';
+import { CyclesContextProvider } from './hooks/useCycles';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Router />
+      <CyclesContextProvider>
+        <GlobalStyle />
+        <Router />
+      </CyclesContextProvider>
     </ThemeProvider>
   );
 }
